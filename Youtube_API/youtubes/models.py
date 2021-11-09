4b7bd06ec17c443eef73d2540e1b8134bccf2c9a
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-09 15:01:50 -0600

    Finished init

--- Youtube_API/youtubes/models.py
+++ Youtube_API/youtubes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    posted_date = models.CharField(max_length=50)
+    comments = []
+
+
+    def __str__(self):
+        return self.title
