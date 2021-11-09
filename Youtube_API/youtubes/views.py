e38c0467d03d042917437d2cadd208caa517effc
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-09 15:21:54 -0600

    Added video serializer

--- Youtube_API/youtubes/views.py
+++ Youtube_API/youtubes/views.py
@@ -1,3 +1,10 @@
+from django.http.response import Http404
 from django.shortcuts import render
+from rest_framework.serializers import Serializer
+from .models import Song
+from .serializers import SongSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
