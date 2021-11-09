4c0f7ab36d816842743a404c64d9d3da396cd596
PeteDillo <91334249+PeteDillo@users.noreply.github.com>
2021-11-09 16:44:57 -0600

    added VideoList class + deleted secrete key from settings.py

--- Youtube_API/Youtube_API/settings.py
+++ Youtube_API/Youtube_API/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9$jv(i@wlc$dy*&=+rjv3w3ax(xl97=jzrn1sr-bi67)a4nk^*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
