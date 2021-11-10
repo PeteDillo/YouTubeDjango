7028f99bb1a10099fd639ad5644f3fa5932337b7
Myles Nathaniel Lister <mylesnlister@Myless-MacBook-Pro.local>
2021-11-10 15:04:12 -0500

    Added Urls.py and paths

--- Youtube_API/youtubes/urls.py
+++ Youtube_API/youtubes/urls.py
@@ -2,5 +2,8 @@
 from . import views
 
 urlpatterns = [
-    
+    path('youtube/', views.CommentList.as_view()),
+    path('reply/', views.ReplyList.as_view()), 
+    path('youtube/<int:pk>/', views.CommentDetail.as_view()),
+    path('reply/<int:pk>/', views.ReplyDetail.as_view())
 ]
